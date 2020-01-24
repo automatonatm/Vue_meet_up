eb173b9e3dcc1f050bcd8905e22357b003a06e88
Automaton <automatonatm@gmail.com>
2020-01-24 17:09:37 +0100

    routing and main pages for app complete

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,9 @@
               v-model="sideNav"
               absolute
               temporary
-              height="400"
               class="overflow-hidden pa-3"
+              height="700">
 
-      >
         <v-list-item>
           <v-list-item-avatar>
             <v-img src="https://randomuser.me/api/portraits/men/78.jpg"></v-img>
@@ -19,17 +18,22 @@
           </v-list-item-content>
         </v-list-item>
 
+
         <v-divider></v-divider>
 
         <v-list dense>
 
-          <v-list-item>
+            <v-list-item
+                    :key="item.title"
+                    :to="item.link"
+                    router
+                    v-for="item in menuItems">
             <v-list-item-icon>
-              <v-icon>supervisor_account</v-icon>
+                <v-icon>{{item.icon}}</v-icon>
             </v-list-item-icon>
 
             <v-list-item-content>
-              <v-list-item-title>View Meetups</v-list-item-title>
+                <v-list-item-title>{{item.title}}</v-list-item-title>
             </v-list-item-content>
           </v-list-item>
         </v-list>
@@ -39,19 +43,29 @@
                 class="hidden-sm-and-up"
                 @click.stop="sideNav = !sideNav">
         </v-app-bar-nav-icon>
-        <v-toolbar-title>DevMeetup</v-toolbar-title>
+
+          <v-toolbar-title>
+              <router-link style="cursor: pointer" tag="span" to="/">DevMeetup</router-link>
+          </v-toolbar-title>
         <v-spacer></v-spacer>
-        <v-toolbar-items class="hidden-xs-only">
-          <v-btn text >
-            <v-icon left>supervisor_account</v-icon>
-            Vue Meetups
+          <v-toolbar-items
+                  :key="item.title"
+                  class="hidden-xs-only"
+                  v-for="item in menuItems">
+
+              <v-btn :to="item.link"
+                     router
+                     text>
+                  <v-icon left>{{item.icon}}</v-icon>
+                  {{item.title}}
           </v-btn>
         </v-toolbar-items>
       </v-toolbar>
 
     </v-card>
-
-    <main></main>
+      <main>
+          <router-view></router-view>
+      </main>
   </v-app>
 </template>
 
@@ -62,7 +76,15 @@ export default {
   name: 'App',
 
   data: () => ({
-    sideNav: false
+      sideNav: false,
+      menuItems: [
+          {icon: 'supervisor_account', title: 'View Meetups', link: '/meetups'},
+          {icon: 'room', title: 'Organize Meetup', link: '/meetup/new'},
+          {icon: 'person', title: 'Profile', link: '/profile'},
+          {icon: 'face', title: 'Sign up', link: '/signup'},
+          {icon: 'lock', title: 'Sign in', link: '/signin'},
+      ]
+
   }),
 };
 </script>
